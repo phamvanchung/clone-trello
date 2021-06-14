e237c716558e6bbd69a7d204676af6e697eaad1a
Chung Phạm Văn <chung14799@gmail.com>
2021-06-14 12:04:07 +0700

    design UI

--- src/App.js
+++ src/App.js
@@ -2,8 +2,60 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="trello-App">
+     <nav className="navbar app">App bar</nav>
+     <nav className="navbar board">Boards</nav>
+     <div className="board-column">
+        <div className="column">
+         <header>Todo</header>
+         <ul>
+             <li><img src="https://img.saostar.vn/w500/2017/12/27/1971045/dr0swo8vwaupjwo.jpg" alt=""></img> </li>
+             <li> tiêu đề danh sách...</li>
+             <li> tiêu đề danh sách...</li>
+             <li> tiêu đề danh sách...</li>
+         </ul>
+         <footer>Add another card</footer>
+         </div>
+
+         <div className="column">
+         <header>Todo</header>
+         <ul>
+             <li><img src="https://img.saostar.vn/w500/2017/12/27/1971045/dr0swo8vwaupjwo.jpg" alt=""></img> </li>
+             <li> tiêu đề danh sách...</li>
+             <li> tiêu đề danh sách...</li>
+             <li> tiêu đề danh sách...</li>
+         </ul>
+         <footer>Add another card</footer>
+         </div>
+         <div className="column">
+         <header>Todo</header>
+         <ul>
+             <li><img src="https://img.saostar.vn/w500/2017/12/27/1971045/dr0swo8vwaupjwo.jpg" alt=""></img> </li>
+             <li> tiêu đề danh sách...</li>
+             <li> tiêu đề danh sách...</li>
+             <li> tiêu đề danh sách...</li>
+         </ul>
+         <footer>Add another card</footer>
+         </div> <div className="column">
+         <header>Todo</header>
+         <ul>
+             <li><img src="https://img.saostar.vn/w500/2017/12/27/1971045/dr0swo8vwaupjwo.jpg" alt=""></img> </li>
+             <li> tiêu đề danh sách...</li>
+             <li> tiêu đề danh sách...</li>
+             <li> tiêu đề danh sách...</li>
+         </ul>
+         <footer>Add another card</footer>
+         </div> <div className="column">
+         <header>Todo</header>
+         <ul>
+             <li><img src="https://img.saostar.vn/w500/2017/12/27/1971045/dr0swo8vwaupjwo.jpg" alt=""></img> </li>
+             <li> tiêu đề danh sách...</li>
+             <li> tiêu đề danh sách...</li>
+             <li> tiêu đề danh sách...</li>
+         </ul>
+         <footer>Add another card</footer>
+         </div>
+     </div>
     </div>
   );
 }
